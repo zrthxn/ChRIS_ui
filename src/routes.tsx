694088f185188d9c5ac94ada9b082b7992817ec9
Alisamar Husain <zrthxn@gmail.com>
2021-06-10 00:29:58 +0530

    Demo for query results

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import PrivateRoute from "./components/common/PrivateRoute";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import FeedsPage from "./pages/Feeds/Feeds";
+import Query from "./pages/Query";
 import { LogIn } from "./pages/LogIn/Login";
 import { NotFound } from "./pages/NotFound/NotFound";
 import SignUp from "./pages/SignUp/SignUp";
@@ -16,6 +17,7 @@ const Routes: React.FunctionComponent = () => {
         <Route exact path="/login" component={LogIn} />
         <Route exact path="/signup" component={SignUp} />
         <PrivateRoute path="/feeds" component={FeedsPage} />
+        <PrivateRoute path="/pacs" component={Query} />
         <PrivateRoute path="/workflows" component={WorkflowsPage} />
         <Route component={NotFound} />
       </Switch>
